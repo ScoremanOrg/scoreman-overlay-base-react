6653836f9f1dd0dfee7ac77eb1746b0ff02c9b16
David Serrano <dserrano@linkare.com>
2021-01-18 00:38:16 +0100

    fix: :bug: fix base url issues
    Now it should on any subpath, both in development mode and production

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import { Route, Switch, Redirect } from 'react-router';
+import { Redirect, Route, Switch } from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { useScoremanBasePath } from './hooks/useScoremanBasePath';
 import { CommentatorsOverlay } from './overlays/commentators/CommentatorsOverlay';
 import { InGameWideScreenOverlay } from './overlays/ingame-widescreen/InGameWidescreenOverlay';
 import './scoreman.css';
 
 const App = () => {
-    return <Router>
-        <Switch>
-            <Route path="/ingame-widescreen">
-                <InGameWideScreenOverlay />
-            </Route>
+    const basePath = useScoremanBasePath();
 
-            <Route path="/commentators">
-                <CommentatorsOverlay />
-            </Route>
+    return basePath ? <div>
+        <div>SCOREMAN OVERLAY "{basePath || 'NO BASE PATH'}"</div>
+        <Router basename={basePath}>
+            <Switch>
+                <Route path="/ingame-widescreen">
+                    <InGameWideScreenOverlay />
+                </Route>
 
-            <Redirect path="/" exact to="/ingame-widescreen"/>
-        </Switch>
-    </Router>
+                <Route path="/commentators">
+                    <CommentatorsOverlay />
+                </Route>
+
+                <Redirect path="/" exact to="/ingame-widescreen" />
+            </Switch>
+        </Router>
+    </div> : <div>loading</div>
 }
 
 export default App;
