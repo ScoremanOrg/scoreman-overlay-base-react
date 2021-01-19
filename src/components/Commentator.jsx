f6f41d7a0db8b2d3e54c3c1cd7ae98a872e05e4e
David Serrano <dserrano@linkare.com>
2021-01-19 16:14:42 +0100

    feat: :sparkles: add support for enabled/disabled commentators

--- src/components/Commentator.jsx
+++ src/components/Commentator.jsx
@@ -5,7 +5,7 @@ export const Commentator = ({ commentator, index, children, ...rest }) => {
     const indexClassName = typeof index === 'number' ? ` commentator-${index}` : '';
 
     function defaultCommentatorRender() {
-        if (commentator) {
+        if (commentator && commentator.enabled) {
             return <>
                 <div className="commentator-name">{commentator.name}</div>
                 <div className="commentator-handle">{commentator.handle}</div>
