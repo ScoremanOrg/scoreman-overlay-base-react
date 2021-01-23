ae48848abb90c4a7fa0b481000807b4f6f20222b
ananas-dev <lucienfiorini@protonmail.com>
2021-01-23 20:03:10 +0100

    fix: :bug: add missing import

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,6 +4,7 @@ import { PlayerCharacter } from './PlayerCharacter';
 import { PlayerCountry } from './PlayerCountry';
 import { PlayerName } from './PlayerName';
 import { PlayerSponsor } from './PlayerSponsor';
+import { PlayerStatus } from './PlayerStatus';
 
 export const Player = ({ player, children, index, ...rest }) => {
     const indexClassName = !!isNaN(index) ? ` player-${index}` : '';
